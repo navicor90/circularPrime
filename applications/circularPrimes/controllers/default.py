5c76d97521600b29df6e226d06a0cf868a8856da
navicor <miltonivanterreno@gmail.com>
2016-01-30 17:51:52 -0300

    Formulario y fixes

--- applications/circularPrimes/controllers/default.py
+++ applications/circularPrimes/controllers/default.py
@@ -9,16 +9,11 @@
 #########################################################################
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
-
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
+    datos = dict('Calcular primos circulares desde 0 hasta':'1000000')
+    form = SQLFORM.dictform(datos)
 
     response.flash = T("Bienvenido")
-    return dict(message=T('Welcome to web2py!'))
+    return dict(form = form)
 
 
 def user():
